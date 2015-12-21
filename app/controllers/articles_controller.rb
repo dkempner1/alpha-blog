b4f11f0c8f01a16965766bb0f605198a8115c97d
dkempner1 <dkempner1@hotmail.com>
2015-12-21 04:30:02 -0700

    add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
-  def new
 
+  def index
+    @articles = Article.all
+  end
+
+  def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
 
   end
 
@@ -11,18 +19,28 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-    	render 'new'
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
   def show
-  	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
 
   end
 
-    private
+  private
   def article_params
     params.require(:article).permit(:title, :description)
   end
 
-  end
+end
