561071d8aba70f0327c71c34358a6d63538ed8aa
dkempner1 <dkempner1@hotmail.com>
2016-01-07 12:50:01 -0700

    stylying update to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -19,11 +20,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
         flash[:success] = "Your account was successfully updated"
         redirect_to articles_path
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
 
