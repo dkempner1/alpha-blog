108862b3aa1009ca848a1f0957e04f8eed28ac10
dkempner1 <dkempner1@hotmail.com>
2016-01-30 04:29:13 -0700

    added destroy action to Category

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :categories, except: [:destroy]
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
